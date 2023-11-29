d1133e32a7ff61937b7a777a4e1694621e222b09
venkata gopichand <venkatagopichand19@gmail.com>
2023-11-29 09:20:13 +0530

    more on props, installin g bootstrap, get css img

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
-// import SunShine from './assets/sunshine.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import dravid from "./assets/dravid.png";
+import kohli from "./assets/kohili.png";
+import laxman from "./assets/laxman.png";
+import rohit from "./assets/rohit.png";
+import sachin from "./assets/sachin.jpg";
+import "./App.css"
 
-const randomNames = ["ram", "laxman", "shivan"];
-
-function getRandomNames(max){
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-    const headerNames = randomNames[getRandomNames(2)]
+function Cricket(props){
     return(
-        <header>
-            {/* <img src = {SunShine} alt = "some image"/> */}
-            <p>
-                My name is {headerNames}
-            </p>
-        </header>
-    );
-}
-
-function Footer(){
-    return(
-        <header>
-            <p>
-                I am from the header component
-            </p>
-        </header>
-    );
+        <li>
+            <p>{props.name}</p>
+            <p>{props.description}</p>
+            <p><img src={props.image} className="img-thumbnail rounded mx-auto d-block" alt={props.name}></img></p>
+        </li>
+    )
 }
 
 function App(){
     return(
-        <div>
-            <Header />
-            <h1> I am from App component</h1>
-            <Footer />
-        </div>
+        <>
+            <h1>Hi i am from app component</h1>
+            <ul>
+                <Cricket 
+                    name = "Sachin"
+                    description = "Sachin is the first player who scores most runs in cricket "
+                    image = {sachin}
+                />
+                <Cricket 
+                    name = "kohil"
+                    description = "Kohil is the first batsman to score 50 cents in the cricket hist"
+                    image = {kohli}
+                />
+                <Cricket 
+                    name = "dravid"
+                    description = "mr Wall in the test cricket"
+                    image = {dravid}
+                />
+                <Cricket 
+                    name = "laxman"
+                    description = "mr smart player"
+                    image = {laxman}
+                />
+                <Cricket 
+                    name = "rohit" 
+                    description = "captian of india"
+                    image = {rohit}
+                />
+            </ul>
+        </>
     )
 }
 
+<ul>
+    <li> a</li>
+</ul>
+
 export default App;
+
+//react whenever we are having muliple tags or elements that should be enclosed with only parent tag
+//otherwise you will get the error fragments
